7180a0148c700b950753398b19fff61618c6d68b
Timothy Cybak <tcybak@2E7.42angouleme.fr>
2025-02-17 15:58:11 +0100

    espace exit good

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 13:45:40 by mdegache          #+#    #+#             */
-/*   Updated: 2025/02/17 14:54:29 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/02/17 15:52:14 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,14 @@
 
 int main(void)
 {
-    char *line;
-    
-    line = NULL;
-    while(1)
-    {
-        line = readline("Minishell:");
-        if(ft_strcmp(line, "exit") == 0)
-            exit(0); // prendre en compte espace avant 
-    }
-    printf("HELLO");
+	char *line;
+	
+	line = NULL;
+	while(1)
+	{
+		line = readline("Minishell:");
+		line = ft_strtrim(line, " ");
+		if(ft_strcmp(line, "exit") == 0)
+			exit(0); // prendre en compte espace avant 
+	}
 }
