e16434685775111fd7435f8f82e0fd376365fcbe
Timothy Cybak <tcybak@2F6.42angouleme.fr>
2025-03-13 18:03:07 +0100

    norm expand

--- Parsing/expand.c
+++ Parsing/expand.c
@@ -6,7 +6,7 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 15:05:45 by mdegache          #+#    #+#             */
-/*   Updated: 2025/03/13 17:44:30 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/03/13 18:01:22 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 char	*quote_out(char *cont, char quote)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 	char	*tmp;
 
 	i = 0;
@@ -35,15 +35,15 @@ char	*quote_out(char *cont, char quote)
 
 char	*get_quote_back(char *data, char quote)
 {
-	int	i;
-	int	j;
-	char *res;
+	int		i;
+	int		j;
+	char	*res;
 
 	i = 0;
 	j = 1;
 	res = ft_calloc(ft_strlen(data) + 3, sizeof(char));
 	res[0] = quote;
-	while(data[i])
+	while (data[i])
 	{
 		res[j] = data[i];
 		i++;
@@ -57,7 +57,7 @@ char	*ft_expand_rest(t_init *init, char *data, char *content, int i)
 {
 	t_list_char	*tmp_env;
 
-	tmp_env  = init->env;
+	tmp_env = init->env;
 	if (data[0] == 39)
 		return (expand_quote(tmp_env, data));
 	if (data[0] != '$')
@@ -72,7 +72,7 @@ char	*ft_expand_rest(t_init *init, char *data, char *content, int i)
 		tmp_env = tmp_env->next;
 	free(data);
 	if (!tmp_env)
-		return(ft_strdup(content));
+		return (ft_strdup(content));
 	while (tmp_env && tmp_env->data[i] && tmp_env->data[i] != '=')
 		i++;
 	return (ft_substr(tmp_env->data, i + 1, ft_strlen(tmp_env->data) - i + 1));
@@ -91,8 +91,7 @@ char	*ft_expand(t_init *init)
 	if (ft_strchr(content, '$'))
 	{
 		if ((content && content[0] == content[ft_strlen(content) - 1]
-			&& content[0] == '"') ||
-				content[0] != 39)
+				&& content[0] == '"') || content[0] != 39)
 		{
 			init->tmp_data = data;
 			data = quote_out(content, '"');
@@ -101,5 +100,5 @@ char	*ft_expand(t_init *init)
 			return (ft_expand_rest(init, data, content, i));
 		}
 	}
-	return (ft_strdup(content));	
+	return (ft_strdup(content));
 }
--- Parsing/expand_2.c
+++ Parsing/expand_2.c
@@ -6,44 +6,44 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 15:46:33 by tcybak            #+#    #+#             */
-/*   Updated: 2025/03/13 16:06:25 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/03/13 18:02:40 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../lib/minishell.h"
 
-char    *verif_simple_quote(char *tmp_data, char *data)
+char	*verif_simple_quote(char *tmp_data, char *data)
 {
-    tmp_data = data;
-    data = get_quote_back(data, 39);
-    free(tmp_data);
-    tmp_data = NULL;
-    return (data);
+	tmp_data = data;
+	data = get_quote_back(data, 39);
+	free(tmp_data);
+	tmp_data = NULL;
+	return (data);
 }
 
-char    *ft_value_var(char *tmp_data, t_list_char *tmp, int i)
+char	*ft_value_var(char *tmp_data, t_list_char *tmp, int i)
 {
-    char *res;
+	char	*res;
 
-    res = ft_substr(tmp->data, i + 1, ft_strlen(tmp->data) - i + 1);
+	res = ft_substr(tmp->data, i + 1, ft_strlen(tmp->data) - i + 1);
 	tmp_data = res;
 	res = get_quote_back(res, 39);
 	free(tmp_data);
-    return (res);
+	return (res);
 }
 
 char	*expand_quote(t_list_char *tmp, char *data)
 {
-	int	i;
-	char *tmp_data;
-	
+	int		i;
+	char	*tmp_data;
+
 	i = 0;
 	tmp_data = data;
 	data = quote_out(data, 39);
 	free(tmp_data);
 	tmp_data = NULL;
 	if (data[0] != '$')
-        return(verif_simple_quote(tmp_data, data));
+		return (verif_simple_quote(tmp_data, data));
 	tmp_data = data;
 	data = quote_out(data, '$');
 	free(tmp_data);
@@ -51,7 +51,7 @@ char	*expand_quote(t_list_char *tmp, char *data)
 	while (tmp && ft_strcmp(data, tmp->data))
 		tmp = tmp->next;
 	if (!tmp)
-		return(verif_simple_quote(tmp_data, data));
+		return (verif_simple_quote(tmp_data, data));
 	free(data);
 	while (tmp && tmp->data[i] && tmp->data[i] != '=')
 		i++;
