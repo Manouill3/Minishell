8e1d68f0b2534e9786c773a1774246fdba57030f
Degache Manuarii <manuarii.degache@gmail.com>
2025-03-20 13:59:46 +0100

    up

--- exec/exec.c
+++ exec/exec.c
@@ -6,7 +6,7 @@
 /*   By: mdegache <mdegache@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/13 10:48:39 by tcybak            #+#    #+#             */
-/*   Updated: 2025/03/20 13:19:20 by mdegache         ###   ########.fr       */
+/*   Updated: 2025/03/20 13:39:50 by mdegache         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,7 +30,6 @@ void	verif_build(t_list_char *lst)
 		printf("Je suis un built-in\n");
 }
 
-
 void	ft_count_cmd(t_list_char *tok , t_fds *fds)
 {
 	t_list_char *tmp;
@@ -45,90 +44,9 @@ void	ft_count_cmd(t_list_char *tok , t_fds *fds)
 	}
 }
 
-void	ft_list_file(t_list_char *tok, t_fds *fds)
-{
-	t_list_char	*tmp;
-	int			count_input;
-	int			count_output;
-
-	fds->file_input = NULL;
-	fds->file_output = NULL;
-	tmp = tok;
-	count_input = 0;
-	count_output = 0;
-	while (tmp)
-	{
-		if (( !ft_strcmp(tmp->data, ">") || !ft_strcmp(tmp->data, ">>")) && tmp->next->data != NULL)
-			count_output++;
-		if (!ft_strcmp(tmp->data, "<"))
-			count_input++;
-		tmp = tmp->next;
-	}
-	fds->file_input = ft_calloc(count_input + 1, sizeof(char *));
-	fds->file_output = ft_calloc(count_output + 1, sizeof(char *));
-}
-
-void	ft_check_file(t_list_char *tok, t_fds *fds)
-{
-	int			i;
-	int			j;
-	t_list_char *tmp;
-
-	tmp = tok;
-	i = 0;
-	j = 0;
-	ft_list_file(tok, fds);
-	while (tmp)
-	{
-		if (!ft_strcmp(tmp->name, "arg"))
-		{
-			if (!ft_strcmp(tmp->prev->data, "<"))
-			{
-				fds->file_input[i] = tmp->data;
-				i++;
-				// if (ft_strcmp(tmp->prev->prev->name, "cmd"))
-				// 	tmp->next->name = "cmd";	
-				// if (access(tmp->data, R_OK | F_OK) != -1)
-				// 	fds->fd_infile = open(tmp->data, O_RDONLY, 0644);
-				// else
-				// 	perror("infile");
-			}
-			if  (!ft_strcmp(tmp->prev->data, ">") || !ft_strcmp(tmp->prev->data, ">>"))
-			{
-				fds->file_output[j] = tmp->data;
-				j++;
-				// if (ft_strcmp(tmp->prev->prev->name, "cmd"))
-				// 	tmp->next->name = "cmd";
-				// if (access(tmp->data, F_OK | W_OK) != -1)
-				// 	fds->fd_outfile = open(tmp->data, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-				// else
-				// {
-					// if (access(tmp->data, F_OK) == -1)
-					// 	fds->fd_outfile = open(tmp->data, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-				// }
-			}
-		}
-		tmp = tmp->next;
-	}
-	i = 0;
-	while(fds->file_input[i])
-	{
-		printf("nom file input =%s\n", fds->file_input[i]);	
-		i++;
-		printf("i = %d\n", i);
-	}
-	i = 0;
-	while(fds->file_output[i])
-	{
-		printf("nom file output =%s\n", fds->file_output[i]);	
-		i++;
-		printf("i = %d\n", i);
-	}
-}
-
 void	ft_exec_cmd(t_init *init)
 {
-	ft_check_file(init->tok, init->fds);
+	handle_redirection(init->tok, init->fds);
 	ft_count_cmd(init->tok, init->fds);
 	// if (init->fds->count_com != 1)
 		// ft_fork();
---
+++ exec/handle_redirection.c
@@ -0,0 +1,121 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   handle_redirection.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdegache <mdegache@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 13:20:56 by mdegache          #+#    #+#             */
+/*   Updated: 2025/03/20 13:59:16 by mdegache         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../lib/minishell.h"
+
+void    handle_redirection(t_list_char *tok, t_fds *fds)
+{
+    ft_check_file(tok, fds);
+    check_access(fds);
+}
+
+void    check_access(t_fds *fds)
+{
+    int i;
+
+    i = 0;
+    while (fds->file_input[i])
+    {
+        if (access(fds->file_input[i], R_OK | F_OK) != -1)
+            fds->fd_infile = open(fds->file_input[i], O_RDONLY, 0644);
+        else
+            perror("infile");
+        i++;
+    }
+    i = 0;
+    while (fds->file_output[i])
+    {
+        if (access(fds->file_output[i], F_OK | W_OK) != -1)
+            fds->fd_outfile = open(fds->file_output[i], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        else
+        {
+            if (access(fds->file_output[i], F_OK) == -1)
+                fds->fd_outfile = open(fds->file_output[i], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        }
+        i++;
+    }
+}
+
+
+
+void	ft_list_file(t_list_char *tok, t_fds *fds)
+{
+	t_list_char	*tmp;
+	int			count_input;
+	int			count_output;
+
+	fds->file_input = NULL;
+	fds->file_output = NULL;
+	tmp = tok;
+	count_input = 0;
+	count_output = 0;
+	while (tmp)
+	{
+		if (( !ft_strcmp(tmp->data, ">") || !ft_strcmp(tmp->data, ">>")) && tmp->next->data != NULL)
+			count_output++;
+		if (!ft_strcmp(tmp->data, "<"))
+			count_input++;
+		tmp = tmp->next;
+	}
+	fds->file_input = ft_calloc(count_input + 1, sizeof(char *));
+	fds->file_output = ft_calloc(count_output + 1, sizeof(char *));
+}
+void    ft_check_ cmd(t_list_char *tok)
+{
+    t_list_char *tmp;
+
+    tmp = tok;
+    while(tmp)
+    {
+        if (!ft_strcmp(tmp->data, "<"))
+        {
+            if (ft_strcmp(tmp->prev->name, "cmd"))
+                tmp->next->name = "cmd";
+        }
+        if  (!ft_strcmp(tmp->data, ">") || !ft_strcmp(tmp->data, ">>"))
+		{
+            if (ft_strcmp(tmp->prev->name, "cmd"))
+                tmp->next->name = "cmd";
+        }
+        tmp = tmp->next;
+    }
+}
+
+void	ft_check_file(t_list_char *tok, t_fds *fds)
+{
+	int			i;
+	int			j;
+	t_list_char *tmp;
+
+	tmp = tok;
+	i = 0;
+	j = 0;
+	ft_list_file(tok, fds);
+	while (tmp)
+	{
+		if (!ft_strcmp(tmp->name, "arg"))
+		{
+			if (!ft_strcmp(tmp->prev->data, "<"))
+			{
+				fds->file_input[i] = tmp->data;
+				i++;
+			}
+			if  (!ft_strcmp(tmp->prev->data, ">") || !ft_strcmp(tmp->prev->data, ">>"))
+			{
+				fds->file_output[j] = tmp->data;
+				j++;
+			}
+		}
+		tmp = tmp->next;
+	}
+}
+
