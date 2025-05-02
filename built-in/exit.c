7790a6d300864e31bda9e1f5ac4ae44502f76cb9
Timothy Cybak <tcybak@2G6.42angouleme.fr>
2025-05-02 14:46:36 +0200

    98 bon

--- built-in/exit.c
+++ built-in/exit.c
@@ -6,7 +6,7 @@
 /*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 13:24:13 by tcybak            #+#    #+#             */
-/*   Updated: 2025/05/01 17:20:12 by tcybak           ###   ########.fr       */
+/*   Updated: 2025/05/02 14:44:42 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,41 @@
 
 void    ft_exit(t_init *param)
 {
-    int nb_arg;
-    int i;
+	int nb_arg;
+	int i;
 
-    i = 0;
-    nb_arg = 0;
-    while (param->tok->cmd[i])
-    {
-        nb_arg++;
-        i++;
-    }
-    if (nb_arg > 2)
-    {
-        param->status = 1;
-        write(2, " too many arguments", 19);
-        exit(param->status);
-    }
-    if (param->tok->cmd[1] != NULL)
-        param->status = ft_atoi(param->tok->cmd[1]);
-    nb_arg = param->status;
-    ft_free_all(param);
-    free_struct(param);
-    printf("exit\n");
-    exit(nb_arg);
+	i = 0;
+	nb_arg = 0;
+	while (param->tok->cmd[i])
+	{
+		nb_arg++;
+		i++;
+	}
+	if (nb_arg > 2)
+	{
+		param->status = 1;
+		write(2, " too many arguments", 19);
+		exit(param->status);
+	}
+	if (param->tok->cmd[1] != NULL)
+	{
+		i = 0;
+		while (param->tok->cmd[1][i])
+		{
+			if (!((param->tok->cmd[1][i] >= 48 && param->tok->cmd[1][i] <= 57) || param->tok->cmd[1][i] == '+' || param->tok->cmd[1][i] == '-'))
+			{
+				param->status = 2;
+				write(2, " numeric argument required",26);
+				printf("exit\n");
+				exit(param->status);
+			}
+			i++;
+		}
+		param->status = ft_atoi(param->tok->cmd[1]);
+	}
+	nb_arg = param->status;
+	ft_free_all(param);
+	free_struct(param);
+	printf("exit\n");
+	exit(nb_arg);
 }
