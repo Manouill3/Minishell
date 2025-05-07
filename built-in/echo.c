cf63946f565241a5498ceedb523cd72d7dd9bdea
Timothy Cybak <tcybak@2F6.42angouleme.fr>
2025-05-07 13:59:40 +0200

    222

--- built-in/echo.c
+++ built-in/echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mdegache <mdegache@student.42.fr>          +#+  +:+       +#+        */
+/*   By: tcybak <tcybak@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 13:35:17 by tcybak            #+#    #+#             */
-/*   Updated: 2025/05/02 08:53:05 by mdegache         ###   ########.fr       */
+/*   Updated: 2025/05/07 13:58:34 by tcybak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,21 @@ void    ft_echo(t_init *param, t_list_char *tok)
     while (ft_strcmp(tok->no_red[i], "echo") || ft_strlen(tok->no_red[i]) <= 0)
         i++;
     i++;
-    while (tok->no_red[i] && (!ft_strcmp(tok->no_red[i], "-n") || (tok->no_red[i][0] == '-' && tok->no_red[i][j] == 'n')))
+    while (tok->no_red[i] && ((!ft_strcmp(tok->no_red[i], "-n")) || (tok->no_red[i][0] == '-' && tok->no_red[i][1] == 'n')))
     {
-        n = 1;
-        if (tok->no_red[i][j])
+        j = 1;
+        while(tok->no_red[i][j])
         {
-            j = 1;
-            i++;
+            if (tok->no_red[i][j] != 'n')
+            {
+                n = 0;
+                ft_print_arg(n, i, tok);
+                return ;
+            }
+            j++;
         }
-        j++;
+        i++;
+        n = 1;
     }
     ft_print_arg(n, i, tok);
 
